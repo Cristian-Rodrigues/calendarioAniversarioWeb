778aaed33056164d88097e5a9a3b89438ed3dfeb
Cristian Rodrigues <cristian.lopes.rodrigues@gmail.com>
2024-01-22 23:39:44 -0300

    Finalizando os detalhes da classe controller, pronto para publicação

--- src/main/java/com/senac/projetoWeb/controller/AniversarianteController.java
+++ src/main/java/com/senac/projetoWeb/controller/AniversarianteController.java
@@ -1,10 +1,19 @@
 package com.senac.projetoWeb.controller;
 
+import org.springframework.ui.Model;
+import com.senac.projetoWeb.model.Aniversariante;
+import com.senac.projetoWeb.service.AniversarianteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AniversarianteController {
+    
+    @Autowired
+    AniversarianteService aniversarianteService;
     
     @GetMapping("/inicio") 
     public String inicio() {      
@@ -12,8 +21,15 @@ public class AniversarianteController {
     }
     
     @GetMapping("/listagem")
-    public String listaForm( ) {
+    public String listaForm(Model model) {
+        model.addAttribute("lista", aniversarianteService.listarTodos());
         return "lista"; 
+    }
+    
+    @PostMapping("/gravar")
+    public String salvando(@ModelAttribute Aniversariante aniversariante, Model model) {
+        aniversarianteService.criar(aniversariante);
+        return "redirect:/listagem";
     }
     
 }
